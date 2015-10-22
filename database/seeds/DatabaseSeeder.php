0411090114e11b6caea61e2da67b2f6bdb8d8e38
Rencie Bautista <rencie.bautista@yahoo.com>
2015-10-22 16:34:00 +0800

    add files

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,9 @@ class DatabaseSeeder extends Seeder
 
         // $this->call(UserTableSeeder::class);
 
+        $this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
         Model::reguard();
     }
 }
