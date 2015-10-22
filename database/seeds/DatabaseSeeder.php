6121b1791893e207ceb6f9340e99aceef4a66a3c
Rencie Bautista <rencie.bautista@yahoo.com>
2015-10-22 16:55:33 +0800

    fix seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,11 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->call(UploadStoreMappingSeeder::class);
+        $this->call(UploadUserSeeder::class);
+        $this->call(UploadSkuMapping::class);
 
-        $this->call(UserTableSeeder::class);
-        $this->call(UserTableSeeder::class);
-        $this->call(UserTableSeeder::class);
         Model::reguard();
     }
 }
